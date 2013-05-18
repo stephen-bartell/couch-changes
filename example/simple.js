e4d98dc8aa6cc09d7cfdafdb48d122cb0c208881
Stephen Bartell <snbartell@gmail.com>
2013-05-18 01:03:24 -0700

    #3 - standarize on url options

--- example/simple.js
+++ example/simple.js
@@ -4,16 +4,7 @@ var es = require('event-stream')
 
 var stream = require('stream')
 
-var opts = {
-    uri: 'http://localhost:5984/test/_changes'
-  , qs: {
-        include_docs: true
-      , since: 'now'
-      , feed: 'continuous'
-    }
-}
-
-var changes = Changes(opts)
+var changes = Changes('http://localhost:5984/test/_changes')
   .pipe(es.split())
   .pipe(es.parse())
   .pipe(es.log())
